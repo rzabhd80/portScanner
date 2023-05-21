b360f3684cd2b3b9271914648df255fbc23339dd
Kaveh HajiNajaf <kavehhn174@gmail.com>
2023-05-21 19:05:52 +0330

    ADD: UDP Checker ADD: TCP Checker ADD: Multi Threading to project

--- include/app.py
+++ include/app.py
@@ -1,8 +1,48 @@
+import udpchecker
+import tcpchecker
+import threading
+import datetime
+import multiprocessing
 
 def app():
-    pass
+    from_port = 1000
+    until_port = 65002
+    num_ports = until_port - from_port
+    num_threads = 4
+    ports_per_thread = num_ports // num_threads
 
+    threads = []
+    for i in range(num_threads):
+        start_port = (i * ports_per_thread) + from_port
+        end_port = ports_per_thread + start_port
+        # thread = threading.Thread(target=tcp_port_scanner, args=(start_port, end_port))
+        thread = threading.Thread(target=tcp_port_scanner, args=(start_port, end_port))
+        threads.append(thread)
+
+    print("Starting Threads")
+    start_time = datetime.datetime.now()
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    stop_time = datetime.datetime.now()
+    time_difference = stop_time - start_time
+    time_difference_seconds = time_difference.total_seconds()
+    print("This action took : ", time_difference_seconds, " seconds !");
+    print("Done")
+
+
+def tcp_port_scanner(start_from, end_port):
+    ip = "127.0.0.1"
+    for i in range(start_from, end_port):
+        # result = udpchecker.check_udp_connection(ip, i)
+        result = tcpchecker.check_tcp_connection(ip, i)
+        if result:
+            print(i, result)
 
 
 if __name__ == "__main__":
-    app()    
+    app()
