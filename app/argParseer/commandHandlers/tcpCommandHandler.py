0f9be77ddc68f00a74367803c7115c6f141007fc
reza <rzxsism80@gmail.com>
2023-05-24 18:21:58 +0330

    tcp port scanner implemented

--- app/argParseer/commandHandlers/tcpCommandHandler.py
+++ app/argParseer/commandHandlers/tcpCommandHandler.py
@@ -11,7 +11,8 @@ class TcpScannerCommandHandler(commandHandler.CommandHandler):
          except (socket.timeout, ConnectionRefusedError, ConnectionResetError):
            return False
     
-    def __tcp_scanner(self,from_port:str,until_port:str)->None:
+    def __tcp_scanner_init_thread(self,ip:str,port:str,from_port:int,until_port:int)->None:
+        self.__check_tcp_connection(ip,port)
         num_ports = until_port - from_port
         num_threads = 4
         ports_per_thread = num_ports // num_threads
@@ -22,23 +23,26 @@ class TcpScannerCommandHandler(commandHandler.CommandHandler):
            end_port = ports_per_thread + start_port
            thread = threading.Thread(target=self.tcp_port_scanner, args=(start_port, end_port))
            threads.append(thread)
-           print("Starting Threads")
-           start_time = datetime.datetime.now()
-
-           for thread in threads:
-              thread.start()
-
-           for thread in threads:
-              thread.join()
-          
-           print("arg parser")
-
-           stop_time = datetime.datetime.now()
-           time_difference = stop_time - start_time
-           time_difference_seconds = time_difference.total_seconds()
-           print("This action took : ", time_difference_seconds, " seconds !");
-           print("Done") 
+        return threads
+            
         
        
-    def handle(slef):
+    def handle(self,ip:str,port:str,from_port:int,until_port:int):
+        threads = self.__tcp_scanner_init_thread(ip,port,from_port,until_port)
+        print("Starting Threads")
+        start_time = datetime.datetime.now()
+
+        for thread in threads:
+              thread.start()
+
+        for thread in threads:
+              thread.join()
+          
+        print("arg parser")
+
+        stop_time = datetime.datetime.now()
+        time_difference = stop_time - start_time
+        time_difference_seconds = time_difference.total_seconds()
+        print("This action took : ", time_difference_seconds, " seconds !");
+        print("Done")
         return super().handle()
