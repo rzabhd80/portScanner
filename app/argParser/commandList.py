5e8eb887ec798d0836f0c082260e82c94dad4d6c
reza <rzxsism80@gmail.com>
2023-05-28 21:57:53 +0330

    code refactored

--- app/argParser/commandList.py
+++ app/argParser/commandList.py
@@ -1,7 +1,10 @@
 import argparse
 
+import argParser.commandHandlers.tcpCommandHandler as tcpHandler
+import argParser.commandHandlers.udpCommandHandler as udpHandler
 
 def init_commands(parser: argparse.ArgumentParser):
     parser.add_argument("ip", type=str, help="ip of the server to be scanned")
-    parser.add_argument("--protocol", dest="protocol", type=str, choices=["tcp", "udp"], default="tcp")
-    parser.add_argument("--ports", dest="ports",nargs="+", type=str,required=True)
+    parser.add_argument("--protocol", dest="protocol", type=str, choices=["tcp", "udp"], default="tcp",action=tcpHandler.TcpScannerCommandHandler)
+    parser.add_argument("--ports", dest="ports",nargs="+", type=str,required=True,action=udpHandler.UdpScannerCommandHandler)
+
